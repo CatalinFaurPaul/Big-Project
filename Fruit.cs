f9662e3e90440fc78fb16dff3ccf8abda95edd02
CatalinFaurPaul <catalinfaurpaul@gmail.com>
2019-06-23 03:04:55 +0300

    Update Fruit.cs

--- Fruit.cs
+++ Fruit.cs
@@ -31,7 +31,7 @@ namespace Snake
         }
         public void Draw()
         {
-            _gameField.Field[_y][_x].Val = "$";
+            _gameField.Field[_y][_x].Val = "@";
             _gameField.Field[_y][_x].Color = ConsoleColor.Yellow;
         }
 
